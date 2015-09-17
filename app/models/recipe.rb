6957532e03c79fe756bf08bbf1363da4396d2941
Nathan Welling <primesitemedia@gmail.com>
2015-09-17 12:08:54 +0700

    config gem Paperclip, add images to assets

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,7 +3,7 @@ class Recipe < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
 
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "400x400!>", thumb: "100x100>" }, default_url: "missing.png"
   # validates_with AttachmentPresenceValidator, attributes: :image
   validates_with AttachmentContentTypeValidator, attributes: :image, content_type: /\Aimage\/.*\Z/
   validates_with AttachmentSizeValidator, attributes: :image, less_than: 1.megabytes
